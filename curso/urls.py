573a0437d378dd8b4cdf3cf6a0a8eb3bc649cc0d
sidneysm <sidneysoaresm@gmail.com>
2015-11-26 12:16:05 -0300

    Definido o templete de exicição dos dados do aluno e terminado o contrele de sessões

--- curso/urls.py
+++ curso/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
 	url(r'^realizamatricula$', views.realiza_matricula),
 	url(r'^buscar_aluno$', views.buscar_aluno),
 	url(r'^lista_alunos$', views.lista_alunos),
-	url(r'^curso$', views.curso),
-	url(r'^login$', views.login),
+	url(r'^cursos$', views.cursos),
+	url(r'^aluno$', views.aluno),
+	url(r'^sair$', views.sair),
 ]
--- curso/views.py
+++ curso/views.py
@@ -1,21 +1,27 @@
-from django.shortcuts import render
-from django.contrib.auth import authenticate
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.sessions.backends.db import SessionStore
 
 from .forms import AlunoForms
 from .models import *
 
 # Create your views here.
+
+
 def index(request):
-	# if request.method == "POST":
-	# 	print("Aluno invalido")
+	# if request.user.is_authenticated():
+	# 	logout(request)
 	return render(request, 'curso/index.html')
+
 
 def buscar_aluno(request):
 	return render(request, 'curso/buscar_aluno.html')
 
-def curso(request):
-	return render(request, 'curso/curso.html')
+
+def cursos(request):
+	cursos = Curso.objects.all()
+	return render(request, 'curso/curso.html', {'cursos': cursos})
+
 
 def realiza_matricula(request):
 	if request.method == "POST":
@@ -31,28 +37,38 @@ def realiza_matricula(request):
 
 
 def lista_alunos(request):
-	alunos = Aluno.objects.filter(nome__contains=request.POST['nome'])
-	return render(request, 'curso/lista_aluno.html', {'alunos':alunos})
+	alunos = Aluno.objects.all()
+	return render(request, 'curso/lista_aluno.html', {'alunos': alunos})
 
-def login(request):
+
+def aluno(request):
+
+	if request.user.is_authenticated():
+		aluno = Aluno.objects.get(user_ptr_id=request.session['_auth_user_id'])
+
+		return render(request, 'curso/aluno_detalhes.html', {'aluno': aluno},)
+
 	if request.method == "POST":
 		print (request.POST['nome'])
-		usuario = authenticate(username=request.POST['nome'], password=request.POST['senha'])
-		print(request.POST)
+		usuario = authenticate(
+			username=request.POST['nome'], password=request.POST['senha'])
+		
 		if usuario is not None:
 			if usuario.is_active:
-			    print("User is valid, active and authenticated")
-			    for x in request.session.keys():
-			    	print (request.session[x])
-
-			    return render(request, 'curso/aluno_detalhes.html', {'aluno': usuario},)
+				login(request, usuario)
+				aluno = Aluno.objects.get(user_ptr_id=request.session['_auth_user_id'])
+				print("User is valid, active and authenticated")
+				return render(request, 'curso/aluno_detalhes.html', {'aluno': aluno},)
 			else:
-			    print("The password is valid, but the account has been disabled!")
+				print("The password is valid, but the account has been disabled!")
 		else:
-		    # the authentication system was unable to verify the username and password
-		    print("The username and password were incorrect.")
+			# the authentication system was unable to verify the username and password
+			print("The username and password were incorrect.")
 		
 	return render(request, 'curso/login.html')
 
-def usuario_detalhe(request):
-	return render(request, 'curso/aluno_detalhes.html')
+def sair(request):
+	logout(request)
+	return redirect('/')
+
+
